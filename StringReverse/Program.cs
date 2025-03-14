3ffce457e0abe78a7bfa45a2a161d18d1afaa25b
kumar6646 <sai.penusoft@gmail.com>
2025-03-14 11:54:14 +0530

    Add project files.

---
+++ StringReverse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name");
+            string a = Console.ReadLine();
+            string b = null;
+            for(int i=a.Length-1;i>=0;i--)
+            {
+                b = b + a[i];
+            }
+            if(a==b)
+            {
+                Console.WriteLine("it is palindrom");
+            }
+            else
+            {
+                Console.WriteLine("not palindrome");
+            }
+               
+        }
+    }
+}
